f99b14fe9eabcfdd61da18c0556f31e6b740c62d
Matthew Spence <mspence5555@gmail.com>
2022-04-24 10:40:46 -0500

    project created with vite: frontend: vue3, rought layout for game board, started player pieces but will now create new branch for that

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+<GameBoard/>
+
+</template>
+
+
+<script setup>
+
+import GameBoard from './components/GameBoard.vue';
+</script>
+
+
+
+
+<style lang="scss">
+#app {
+    
+}
+</style>
---
+++ frontend/src/components/GameBoard.vue
@@ -0,0 +1,564 @@
+<template>
+<body>
+
+	<div class="responsive">
+
+		<div class="mainSquare">
+
+			<div class="pos-top row top">
+                
+                
+				<div id="freeparking" class="square2 "><span class="corner corner1">free <br /> parking</span>
+                </div>
+                
+                
+				<div class="square1">
+					<div class="header header-top red"></div>
+					<div class="firstLine firstLine-top rotation2">kentucky <br /> avenue</div>
+				</div>
+
+				<div class="square1">		
+					<div class="firstLine firstLine-top no-color rotation2">chance</div>
+				</div>
+
+				<div id="clickable" data-id="indianaave" class="square1 ">
+					<div data-id="indianaave" class="header header-top red"></div>
+					<div data-id="indianaave" class="firstLine firstLine-top rotation2">indiana <br /> avenue</div>
+				</div>
+
+				<div id="clickable" data-id="illinoisave" class="square1 ">
+					<div data-id="illinoisave" class="header header-top red"></div>
+					<div data-id="illinoisave" class="firstLine firstLine-top rotation2">illinios <br /> avenue</div>
+				</div>
+
+				<div id="clickable" data-id="borailroad" class="square1 ">
+					<div data-id="borailroad" class="firstLine firstLine-top no-color rotation2">b. & o. <br /> railroad</div>
+				</div>
+
+				<div id="clickable" data-id="atlanticave" class="square1 ">
+					<div data-id="atlanticave" class="header header-top yellow"></div>
+					<div data-id="atlanticave" class="firstLine firstLine-top rotation2">atlantic <br /> avenue</div>
+				</div>
+
+				<div id="clickable" data-id="ventnorave" class="square1 ">
+					<div data-id="ventnorave" class="header header-top yellow"></div>
+					<div data-id="ventnorave" class="firstLine firstLine-top rotation2">ventnor <br /> avenue</div>
+				</div>
+
+				<div id="clickable" data-id="waterworks" class="square1 ">
+                    <!-- waterworks not in this.vueopoly.properties -->
+					<div data-id="waterworks" class="firstLine firstLine-top no-color rotation2">water <br /> works</div>
+				</div>
+
+				<div id="clickable" data-id="marvingardens" class="square1 ">
+					<div data-id="marvingardens" class="header header-top yellow"></div>
+					<div data-id="marvingardens" class="firstLine firstLine-top rotation2">marvin <br /> gardens</div>
+				</div>
+
+				<div id="clickable" data-id="gotojail" class="square2 "><span data-id="gotojail" class="corner corner2">go to<br />jail</span></div>
+			</div>
+			
+			<div class="row center">
+				<div class="pos-left square2">
+
+					<div id="clickable" data-id="newyorkave" class="squareSide ">
+						<div data-id="newyorkave" class="headerSide header-left orange"></div>
+						<div data-id="newyorkave" class="firstLine firstLine-left rotation1">new york <br /> avenue</div>
+					</div>
+
+					<div id="clickable" data-id="tennesseeave" class="squareSide ">
+						<div data-id="tennesseeave" class="headerSide header-left orange"></div>
+						<div data-id="tennesseeave" class="firstLine firstLine-left rotation1">tennessee <br /> avenue</div>
+					</div>
+					<div id="clickable" data-id="communitychesttwo" class="squareSide ">						
+						<div data-id="communitychesttwo" class="firstLine firstLine-left no-color rotation1">community<br /> chest</div>
+					</div>
+
+					<div id="clickable" data-id="stjamesplace" class="squareSide ">
+						<div data-id="stjamesplace" class="headerSide header-left orange"></div>
+						<div data-id="stjamesplace" class="firstLine firstLine-left rotation1">st.james <br />place</div>
+					</div>
+
+					<div id="clickable" data-id="pennsylvaniarailroad" class="squareSide ">						
+						<div data-id="pennsylvaniarailroad" class="firstLine firstLine-left no-color rotation1">pennsylvania<br> railroad</div>
+					</div>
+					<div id="clickable" data-id="virginiaave" class="squareSide ">
+						<div data-id="virginiaave" class="headerSide header-left purple"></div>
+						<div data-id="virginiaave" class="firstLine firstLine-left rotation1">virginia<br /> avenue</div>
+					</div>
+					<div id="clickable" data-id="statesave" class="squareSide ">
+						<div data-id="statesave" class="headerSide header-left purple"></div>
+						<div data-id="statesave" class="firstLine firstLine-left rotation1">states<br /> avenue</div>
+					</div>
+					<div id="clickable" data-id="electriccompany" class="squareSide ">						
+						<div data-id="electriccompany" class="firstLine firstLine-left no-color rotation1">electric<br />company</div>
+					</div>
+					<div id="clickable" data-id="stcharlesplace" class="squareSide ">
+						<div data-id="stcharlesplace" class="headerSide header-left purple"></div>
+						<div data-id="stcharlesplace" class="firstLine firstLine-left rotation1">st. charles<br />place</div>
+					</div>
+				</div>
+
+				<div class="square9">
+					<div class="card-box card-blue">
+						<div class="card-blue-inside"></div>
+					</div>
+					<div class="logoBox">
+						<span class="logoName">vueopoly</span>
+					</div>
+					<div class="card-box card-orange">
+						<div class="card-orange-inside"></div>						
+					</div>
+				</div>
+
+				<div class="pos-right square2">
+					<div id="clickable" data-id="pacificave" class="squareSide ">
+						<div data-id="pacificave" class="headerSide header-right green"></div>
+						<div data-id="pacificave" class="firstLine firstLine-right rotation3">pacific<br /> avenue</div>
+					</div>
+					<div id="clickable" data-id="northcarolinaave" class="squareSide ">
+						<div data-id="northcarolinaave" class="headerSide header-right green"></div>
+						<div data-id="northcarolinaave" class="firstLine firstLine-right rotation3">north<br />carolina<br />avenue</div>
+					</div>
+					<div id="clickable" data-id="communitychestthree" class="squareSide ">						
+						<div data-id="communitychestthree" class="firstLine firstLine-right no-color rotation3">community<br />chest</div>
+					</div>
+					<div id="clickable" data-id="pennsylvaniaave" class="squareSide ">
+						<div data-id="pennsylvaniaave" class="headerSide header-right green"></div>
+						<div data-id="pennsylvaniaave" class="firstLine firstLine-right rotation3">pennsylvania<br /> avenue</div>
+					</div>
+					<div id="clickable" data-id="shortlinerailroad" class="squareSide ">						
+						<div data-id="shortlinerailroad" class="firstLine firstLine-right no-color rotation3">short<br /> line</div>
+					</div>
+                    
+                    <div id="clickable" data-id="chancethree" class="squareSide ">
+                        <div data-id="chancethree" class="firstLine firstLine-right no-color rotation3">chance</div>
+                    </div>
+                    
+					<div id="clickable" data-id="parkplace" class="squareSide ">						
+						<div data-id="parkplace" class="headerSide header-right blue"></div>
+						<div data-id="parkplace" class="firstLine firstLine-right rotation3">park<br /> place</div>
+					</div>
+					<div id="clickable" data-id="luxerytax" class="squareSide">						
+						<div data-id="luxerytax" class="firstLine firstLine-right no-color rotation3">luxury<br /> tax</div>
+					</div>
+					<div id="clickable" data-id="boardwalk" class="squareSide ">
+						<div data-id="boardwalk" class="headerSide header-right blue"></div>
+						<div data-id="boardwalk" class="firstLine firstLine-right rotation3">boardwalk<br /> avenue</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="pos-bottom row top">
+                
+                <!-- <div id="clickable" data-id="jail" class="square2 ">
+					<span data-id="jail" class="corner4-left">just</span>
+					<div data-id="jail" class="squareCorner">
+						<span data-id="jail" class="corner corner4">in <br />jail</span>
+					</div>
+                    
+					<span data-id="jail" class="corner4-bottom">visiting</span>
+				</div> -->
+
+				<div id="clickable" data-id="jail" class="square2 ">
+					<span data-id="jail" class="corner4-left">just</span>
+
+					<div data-id="injail" class="squareCorner">
+						<span data-id="injail" class="corner corner4">in <br />jail</span>
+					</div>
+
+					<span data-id="jail" class="corner4-bottom">visiting</span>
+				</div>
+
+				<div id="clickable" data-id="connecticutave" class="square1 ">
+					<div data-id="connecticutave" class="header header-bottom lightblue"></div>
+					<div data-id="connecticutave" class="firstLine firstLine-bottom">connecticut avenue</div>
+				</div>
+				<div id="clickable" data-id="vermontave" class="square1 ">
+					<div data-id="vermontave" class="header header-bottom lightblue"></div>
+					<div data-id="vermontave" class="firstLine firstLine-bottom">vermont<br /> avenue</div>
+				</div>
+				<div id="clickable" data-id="chance" class="square1 ">
+					<div data-id="chance" class="firstLine firstLine-bottom no-color">chance</div>
+				</div>
+				<div id="clickable" data-id="orientalave" class="square1 ">
+					<div data-id="orientalave" class="header header-bottom lightblue"></div>
+					<div data-id="orientalave" class="firstLine firstLine-bottom">oriental<br /> avenue</div>
+				</div>
+				<div id="clickable" data-id="readingrailroad" class="square1 ">					
+					<div data-id="readingrailroad" class="firstLine firstLine-bottom no-color">reading<br /> railroad</div>
+				</div>
+				<div id="clickable" data-id="incometax" class="square1 ">
+					<div data-id="incometax" class="firstLine firstLine-bottom no-color">income<br /> tax</div>
+				</div>
+				<div id="clickable" data-id="balticave" class="square1 ">
+					<div data-id="balticave" class="header header-bottom brown"></div>
+					<div data-id="balticave" class="firstLine firstLine-bottom">baltic<br /> avenue</div>
+				</div>
+				<div id="clickable" data-id="communitychest" class="square1 ">
+					<div data-id="communitychestThree" class="firstLine firstLine-bottom no-color">community<br /> chest</div>
+				</div>
+				<div id="clickable" data-id="mediterraneanave" class="square1 ">
+					<div data-id="mediterraneanave" class="header header-bottom brown"></div>
+					<div data-id="mediterraneanave" class="firstLine firstLine-bottom">mediter-<br /> ranean <br /> avenue</div>
+				</div>
+				<div id="go" class="square2 ">
+					<span class="corner corner3">collect<br />$200 salary<br />as you pass</span>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</template>
+
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+
+    testDom();
+});
+let playerPieceObj = {
+    player1: {
+        color: 'red',
+        position: {
+            go: '90% 0 0 90%'
+        }
+    },
+    player2: {
+        color: 'purple',
+        position: {
+            go: '90% 0 0 80%'
+        }
+    },
+    player3: {
+        color: 'black',
+        position: {
+            go: '90% 0 0 70%'
+        }
+    },
+    player4: {
+        color: 'green',
+        position: {
+            go: '90% 0 0 60%'
+        }
+    },
+    player5: {
+        color: 'brown',
+        position: {
+            go: '90% 0 0 50%'
+        }
+    },
+    player6: {
+        color: 'yellow',
+        position: {
+            go: '90% 0 0 40%'
+        }
+    },
+    player7: {
+        color: 'green',
+        position: {
+            go: '90% 0 0 30%'
+        }
+    },
+    player8: {
+        color: 'gray',
+        position: {
+            go: '90% 0 0 20%'
+        }
+    }
+}
+
+
+function testDom() {
+    
+    let position = 'go';
+    let playerP;
+    
+    for(let i = 1; i < 9; i++) {
+
+        playerP = document.createElement('span');
+        playerP.classList.add('player-piece');
+
+        // console.log(playerPieceObj['player' + `${i}`]);
+
+        playerP.style.inset = playerPieceObj['player' + `${i}`].position[`${position}`];
+        playerP.style.backgroundColor = playerPieceObj['player' + `${i}`].color;
+        
+        
+
+        document.getElementById(position).append(playerP);
+        
+    };
+
+    
+
+    // console.log(domSquare)
+    
+}
+
+</script>
+
+
+<style lang="scss">
+.player-piece {
+    position: absolute;
+    
+    width: 10%;
+    height: 10%;
+    
+    
+}
+@media (min-width: 480px) {
+
+
+}    
+
+div {
+	box-sizing: border-box;
+	text-transform: uppercase;
+}
+.responsive {
+    background: #cde6d0;
+    width: 80vw;
+    height: 80vw;
+    margin: 10px auto;
+}
+.mainSquare {
+	height: 100%;
+	outline: 1px dashed #c2dec5;
+	outline-offset: -13px;
+}
+.row {
+	width: 100%;
+	display: flex;
+}
+.top {
+	height: 15.384615385%;
+}
+.center {
+	height: 69.23076923%;
+}
+.square1 {
+	outline: 1px solid black;
+	flex-grow: 1;
+	position: relative;
+}
+.square2 {
+	flex-grow: 2;
+	outline: 1px solid black;
+	display: flex;
+	flex-direction: column;
+	position: relative;
+}
+.square9 {
+	flex-grow: 9;
+	outline: 1px solid black;
+}
+.squareSide {
+	outline: 1px solid black;
+	width: 100%;
+	flex-grow: 1;
+	position: relative;
+}
+.header {
+	height: 21%;
+	position: absolute;
+	outline: 2px solid black;
+	background: grey;
+	/* width: 100%; */
+}
+.headerSide {
+	/* height: 100%; */
+	width: 21%;
+	position: absolute;
+	outline: 2px solid black;
+}
+.header-top {
+	bottom: 1px;
+	left: 1px;
+	right: 1px;
+}
+.header-bottom {
+	top: 1px;
+	left: 1px;
+	right: 1px;
+}
+.header-left {
+	top: 1px;
+	bottom: 1px;
+	right: 1px;
+	background: grey;
+}
+.header-right {
+	top: 1px;
+	bottom: 1px;
+	left: 1px;
+	background: grey;
+}
+.firstLine {
+	position: absolute;
+	font-size: 0.7vw;
+	font-weight: 500;
+	line-height: 1vw;
+	text-align: center;
+}
+.firstLine-top {
+	left: 1px;
+	right: 1px;
+	bottom: 30%;
+	height: 10%;
+}
+.firstLine-top.no-color {
+	bottom: 12%;
+}
+.firstLine-left {
+	top: 0px;
+	bottom: 0px;
+	right: 42%;
+}
+.firstLine-left.no-color {
+	right: 23%;
+}
+.firstLine-right {
+	top: 0;
+	bottom: 0;
+	left: 42%;
+}
+.firstLine-right.no-color {
+	left: 23%;
+}
+.firstLine-bottom {
+	left: 0;
+	right: 0;
+	top: 30%;
+}
+.firstLine-bottom.no-color {
+	top: 12%;
+}
+.red {
+  background-color: #ed1b24;
+}
+.yellow {
+  background-color: #fef200;
+}
+.lightblue {
+  background-color: #aae0fa;
+}
+.brown {
+  background-color: #955436;
+}
+.blue {
+  background-color: #0072bb;
+}
+.green {
+  background-color: #1fb25a;
+}
+.orange {
+  background-color: #f7941d;
+}
+.purple {
+  background-color: #d93a96;
+}
+.rotation1 {
+  transform: rotate(90deg);
+}
+.rotation2 {
+  transform: rotate(180deg);
+}
+.rotation3 {
+  transform: rotate(-90deg);
+}
+.logoBox {
+	width: 46%;
+	background: #ed1b24;
+	position: absolute;
+	transform: rotateZ(-45deg) translateX(-27%) translateY(237%);
+	border: 3px solid black;
+	text-align: center;
+	box-shadow: inset 0px 2px 12px 0px white;
+}
+.logoName {
+	font-size: 7.1vw;
+	color: white;
+	font-family: futura;
+	text-shadow: -4px 3px 0px black;
+	-webkit-text-fill-color: white; /* Will override color (regardless of order) */
+	-webkit-text-stroke-width: 2px;
+	-webkit-text-stroke-color: #cfc2c3;
+}
+.card-box {
+	position: absolute;
+	width: 15%;
+	height: 9vw;
+}
+.card-blue {
+	background: linear-gradient(#4ccaf4, #a3dff9);
+	transform: rotateZ(-45deg) translateX(-13%) translateY(66%);
+}
+.card-blue-inside {
+    background: linear-gradient(#a3dff9, #4ccaf4);
+    width: 94%;
+    height: 94%;
+    margin: 0.3vw auto;
+    position: relative;
+}
+.card-orange {
+	background: linear-gradient(#f99120, #f57420);
+	transform: rotateZ(-45deg) translateX(-13%) translateY(610%);
+}
+.card-orange-inside {
+    background: linear-gradient(#f57420, #f99120);
+    width: 94%;
+    height: 94%;
+    margin: 0.3vw auto;
+    position: relative;
+}
+.corner {
+	position: absolute;
+	text-align: center;
+	line-height: 8vw;
+	font-size: 1.2vw;
+	font-weight: 500;
+}
+.corner1 {
+    transform: rotateZ(135deg) translateX(-66%) translateY(-15%);
+}
+.corner2 {
+    transform: rotateZ(-135deg) translateX(-30%) translateY(23%);
+    line-height: 9vw;
+}
+.corner3 {
+    transform: rotateZ(-45deg) translateX(-14%) translateY(26%);
+    line-height: 1.4vw;
+    font-size: 1vw;
+}
+.corner4 {
+    transform: rotateZ(45deg) translateX(-9%) translateY(-31%);
+    line-height: 7vw;
+}
+.squareCorner {
+	height: 65%;
+	width: 65%;
+	border: 2px solid black;
+	right: -1px;
+	top: -1px;
+	position: absolute;
+}
+.corner4-left {
+	position: absolute;
+	transform: rotateZ(90deg) translateX(80%) translateY(-36%);
+	font-size: 1.2vw;
+	font-weight: 500;
+}
+.corner4-bottom {
+	position: absolute;
+	transform: translateX(112%) translateY(720%);
+	font-size: 1.2vw;
+	font-weight: 500;
+}
+</style>
